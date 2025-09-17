72f933dca80207cfd8415e88477b41021ed5ab73
Yara Yaghi <yyaghi@gmu.edu>
2025-09-17 15:54:39 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,8 @@
 print("This is my first Git repository on GitHub.")
 
+print("Hello Earth!")
+print()
+
 def print_message(name='GitHub'):
   print(f"Welcome to the {name} tutorial!")
 print_message()
